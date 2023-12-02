ff60f25a439d94b2af586dd63343a9780d633d53
MaxikForever <69801190+MaxikForever@users.noreply.github.com>
2023-12-02 20:23:12 +0300

    KAN-4 Added Identity Tables to db

--- src/Clothick.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Clothick.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,17 +1,19 @@
+using Clothick.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Clothick.Infrastructure.Persistence;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
-
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        base.OnModelCreating(modelBuilder);
     }
 }
