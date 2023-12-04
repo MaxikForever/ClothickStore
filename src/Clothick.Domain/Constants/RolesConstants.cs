c89b3c1aeb129ee20fb0e67b36ee09181960bc5c
MaxikForever <69801190+MaxikForever@users.noreply.github.com>
2023-12-04 22:01:57 +0300

    Added RolesConstants

---
+++ src/Clothick.Domain/Constants/RolesConstants.cs
@@ -0,0 +1,8 @@
+namespace Clothick.Domain.Constants;
+
+public static class RolesConstants
+{
+    public const string Guest = "Guest";
+    public const string User = "User";
+    public const string Admin = "Admin";
+}
